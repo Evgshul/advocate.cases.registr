0c831066fb2db4d00b16b6637db831c9d5a0df1f
jevgenijs sulgins <jevgenijs.sulgins@accenture.com>
2022-03-21 14:43:18 +0200

    Initial Commit

---
+++ src/main/java/com/lv/adv/cass/regstr/AdvCasesRegistrApplication.java
@@ -0,0 +1,13 @@
+package com.lv.adv.cass.regstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvCasesRegistrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvCasesRegistrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lv/adv/cass/regstr/AdvCasesRegistrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv.adv.cass.regstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvCasesRegistrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
